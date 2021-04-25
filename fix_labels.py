f5805234dcc0ccb2027efbce55332a366833acaa
Colman Glagovich <oliver@colM1.local>
2021-04-25 19:16:01 -0400

    asdf

--- fix_labels.py
+++ fix_labels.py
@@ -1,8 +1,8 @@
 import numpy as np
 import os
-import argparser
+import argparse
 
-parser = argparser.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument('--dir')
 args = parser.parse_args()
 
